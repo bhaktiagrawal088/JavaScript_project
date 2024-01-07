cb97eb3f3fbfe408bdfa76c15d81ef78851f434f
bhaktiagrawal088 <bhaktiagrawal286@gmail.com>
2024-01-07 16:17:34 +0530

    Music Player

---
+++ Music Player/music.js
@@ -0,0 +1,33 @@
+let progress = document.getElementById('progress')
+let song = document.getElementById('song')
+let CrtlIcon = document.getElementById('CrtlIcon')
+
+song.onloadedmetadata = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+function playPause(){
+    if(CrtlIcon.classList.contains('fa-pause')){
+        song.pause();
+        CrtlIcon.classList.remove('fa-pause');
+        CrtlIcon.classList.add('fa-play');
+    }
+    else{
+        song.play();
+        CrtlIcon.classList.add('fa-pause');
+        CrtlIcon.classList.remove('fa-play');
+    }
+}
+
+if (song.play()){
+    setInterval(()=>{
+        progress.value = progress.currentTime;
+    },500)
+}
+
+progress.onchange = function(){
+    song.play();
+    song.currentTime = progress.value; 
+    CrtlIcon.classList.add('fa-pause');
+    CrtlIcon.classList.remove('fa-play')
+}
