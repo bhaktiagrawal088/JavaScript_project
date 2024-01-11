5723e70fc9e1b2bc08c4ad20324e6b73c84d76b0
bhaktiagrawal088 <bhaktiagrawal286@gmail.com>
2024-01-11 16:22:34 +0530

    Toast Notification

---
+++ Toast Notification/notif.js
@@ -0,0 +1,24 @@
+
+let toastBox = document.getElementsById('toastBox');
+
+let successmsg = '<i class="fa-solid fa-circle-check"></i> Successfully submitted';
+let errormsg = '<i class="fa-solid fa-circle-xmark"></i> Please fix the error !';
+let invalidmsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid Input,check again !';
+
+function showToast(msg){
+    let toast = document.createElement('div');
+    toast.classList.add(toast);
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
+
+    if(msg.includes('error')){
+        toast.classList.add('error')
+    }
+    else if(msg.includes('invalid')){
+        toast.classList.add('invalid')
+    }
+
+    setTimeout(() =>{
+        toast.remove();
+    },6000)
+}
